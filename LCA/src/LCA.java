461d80f9595083160a73d6fa093f3b9e4a241a9b
grant <grant@192.168.0.109>
2019-10-08 18:48:00 +0100

    Continued working on findingLCA function, added comments to function & deleted N (instance variable for Node class)

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -5,25 +5,29 @@ public class LCA {
 		
 		private int value; // The data in the node - changed to int for initial building of functions
 		private Node left, right; // Left & Right subtree node
-		private int N; // Number of nodes in subtree, not sure if this will be needed
 		
-		public Node(int val, int N) {
+		public Node(int val) {
 			this.value = val;
-			this.N= N;
 			this.left = null;
 			this.right = null;
 		}
+	}
 		
 	Node root;
 	Node findingLCA(int input1, int input2) {
-		return findingLCA(root, input1, input2);
+		return findingLCA(root, input1, input2); //The parameters are the 2 input numbers & the top of the tree (root)
 	}
 	
-	Node findingLCA(Node node, int input1, int input2) {
+	Node findingLCA(Node node, int input1, int input2) { //This function will use recursion to find the LCA of 2 inputs.
+		
+		if(node == null) return node;
+		
+		if(node.value == input1 || node.value == input2) return node; //If either of the input values are reched in the tree then the
+		//node is returned as the LCA cannot be in a subtree of one of the inputs.
+		
 		return null;
 	}
-	
-}
+
 
 
 	
