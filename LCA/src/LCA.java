063630fb355c2a1cd1adb53795675d62784c237e
grant <grant@192.168.0.109>
2019-10-08 18:35:49 +0100

    Started writing findingLCA functions, tidied Node class

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -1,19 +1,30 @@
 
-public class LCA <Value extends Comparable<Value>>{
+public class LCA {
 	
-	public class Node {
+	class Node {
 		
-		private Value value; // The data in the node
-		private Node left; // Left subtree node
-		private Node right; // Right subtree node
-		private int N; // Number of nodes in subtree
+		private int value; // The data in the node - changed to int for initial building of functions
+		private Node left, right; // Left & Right subtree node
+		private int N; // Number of nodes in subtree, not sure if this will be needed
 		
-		public Node(Value val, int N) {
+		public Node(int val, int N) {
 			this.value = val;
 			this.N= N;
 			this.left = null;
 			this.right = null;
 		}
 		
+	Node root;
+	Node findingLCA(int input1, int input2) {
+		return findingLCA(root, input1, input2);
 	}
+	
+	Node findingLCA(Node node, int input1, int input2) {
+		return null;
+	}
+	
+}
+
+
+	
 }
