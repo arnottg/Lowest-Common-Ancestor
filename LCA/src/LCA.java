e5209386ae106e0928f017294c36cbf076b475f5
grant <grant@192.168.0.109>
2019-10-08 17:35:05 +0100

    Created basic Node class & constructor

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -1,4 +1,19 @@
 
-public class LCA {
-
+public class LCA <Value extends Comparable<Value>>{
+	
+	public class Node {
+		
+		private Value value; // The data in the node
+		private Node left; // Left subtree node
+		private Node right; // Right subtree node
+		private int N; // Number of nodes in subtree
+		
+		public Node(Value val, int N) {
+			this.value = val;
+			this.N= N;
+			this.left = null;
+			this.right = null;
+		}
+		
+	}
 }
