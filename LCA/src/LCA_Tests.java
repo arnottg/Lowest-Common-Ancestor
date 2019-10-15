65c99aaafda29a088e17b0d8f94817e2c9864eba
grant <grant@10.6.24.205>
2019-10-15 12:19:40 +0100

    Added Test Functions

---
+++ LCA/src/LCA_Tests.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+class LCA_Tests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testEmptyTree() {
+		
+	}
+	
+	@Test
+	void testSingleNode() {
+		
+	}
+	
+	@Test
+	void testTwoNodes() {
+		
+	}
+	
+	@Test
+	void testStandardTree() {
+		
+	}
+	
+}
