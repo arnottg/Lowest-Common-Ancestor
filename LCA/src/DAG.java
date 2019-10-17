e48fe1b5ce3633f582da68a1aa648eba041d625b
grant <grant@10.6.24.205>
2019-10-17 20:35:40 +0100

    Added findingLCA function - tests to follow

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -1,12 +1,13 @@
 
 public class DAG {
+	
 	private int V;//Number of vertices in graph
 	private int E;//Number of edges in graph
 	private int[][] adjList; //2D array adjacency list for vertex v
 	private int[] outdegree;//outdegree of vertex v
 	private int[] indegree; // indegree of vertex v
 	private int[] visited;  //2D array of visited vertices
-   
+	
     //constructor to initialize and empty graph with size V
     public DAG(int V){
 		if(V < 0){
@@ -39,7 +40,7 @@ public class DAG {
 	
 	//throws illegal exception if the vertex put in is invalid
 	private void checkVertex(int v){
-		if((v<0)||(v>=V)){ //Vertex must be between 0 and V or is out of bounds
+		if((v < 0)||(v >= V)){ //Vertex must be between 0 and V or is out of bounds
     		throw new IllegalArgumentException("Input vertex is out of bounds, v must be between 0 and " + V);
 		}
 	}
@@ -92,7 +93,7 @@ public class DAG {
     	return adj;
     }
     
-  //returns true if the graph contains a cycle, else false
+    //returns true if the graph contains a cycle, else false
     public boolean hasCycle(){
     	boolean hasCycle = false; //A boolean tracking whether or not the DAG has a cycle
     	int count = 0; //Counter token to be incremented & used to find cycle
@@ -110,6 +111,49 @@ public class DAG {
     	}
     	return hasCycle;
     }
-
+    
+    //Found some nice code for LCA of DAG online, adjustments will be necessary to get my tests to work
+    //This public function is used to find the LCA in a DAG
+    public int findingLCA(int v, int w){
+    	checkVertex(v);
+    	checkVertex(w);
+    	if(E > 0 && !hasCycle()){ //If the DAG does not contain a cycle and has edges > 0, we can attempt to find the LCA
+    		int[] vArray = new int[E];
+    		int[] wArray = new int[E];
+    		boolean[] vMarked = new boolean[V];
+    		boolean[] wMarked = new boolean[V];
+    		int vCount =0;
+    		int wCount = 0;
+    		vArray[vCount] = v;
+    		wArray[wCount] = w;
+    		for(int j=0; j<V;j++){ //mark all vertices as not been visited yet
+    			vMarked[j] = false;
+    			wMarked[j] = false;
+    		}
+    		for(int i =0; i < V; i++){
+    			vMarked[v] = true;
+    			wMarked[w] = true;
+    			for(int j = 0; j < V; j++){
+    				if(adjList[i][j] == 1 && vMarked[i]){
+    					vCount++;
+    					vArray[vCount]=j;
+    					vMarked[j]=true;
+    				}
+    				if(adjList[i][j] == 1 && wMarked[i]){
+    					wCount++;
+    					wArray[wCount]=j;
+    					wMarked[j]=true;
+    				}
+    				if(wArray[wCount] == vArray[vCount]){
+    					return wArray[wCount];
+    				}
+    			}
+    		}
+    		return -1;//returns -1 if no ancestor found
+    	}
+    	else{
+    		throw new IllegalArgumentException("This graph is not an acyclic graph - it may not contain any edges or have a cycle");
+    	}
+    }
     
 }
