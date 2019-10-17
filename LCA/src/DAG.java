0f6e840d0f81752e8516fe6ae2ca9cd93d5944d0
grant <grant@192.168.0.109>
2019-10-17 22:01:15 +0100

    Updated DAG and added test

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -1,7 +1,7 @@
 
 public class DAG {
 	
-	public int V;//Number of vertices in graph
+	private int V;//Number of vertices in graph
 	private int E;//Number of edges in graph
 	private int[][] adjList; //2D array adjacency list for vertex v
 	private int[] outdegree;//outdegree of vertex v
@@ -30,69 +30,77 @@ public class DAG {
 	    }
 	}
 	
-	//returns number of vertices in DAG
-	public int Vertices(){
+	//returns number of vertices in DAG - Used for tests
+	public int vertices(){
 		return V;
 	}
 	
-	//returns number of edges in DAG
-	public int Edges(){
+	//returns number of edges in DAG - Used for tests
+	public int edges(){
 		return E;
 	}
 	
 	//throws illegal exception if the vertex put in is invalid
-	private void checkVertex(int v){
-		if((v < 0)||(v >= V)){ //Vertex must be between 0 and V or is out of bounds
-    		throw new IllegalArgumentException("Input vertex is out of bounds, v must be between 0 and " + V);
+	public int checkVertex(int v){
+		if((v < 0)||(v >= V)){ //Vertex must be between 0 and V-1 or is out of bounds
+    		return -1; //Returns -1 if v is out of bounds
 		}
+		return 1; //returns 1 if v is in bounds
 	}
     
   //adds directed edge from v to w
-	public void addEdge(int v, int w){
+	public int addEdge(int v, int w){
 		//Checking if vertices are in bounds
-		checkVertex(v);
-		checkVertex(w);
-		adjList[v][w] = 1; //Adds the edge to the adjacent list
-		indegree[w]++; //The number of edges entering vertex w is increased by 1
-		outdegree[v]++; //The number of edges leaving vertex v is decreased by 1
-		E++;
+		if(checkVertex(v) == 1 && checkVertex(w) == 1) {
+			adjList[v][w] = 1; //Adds the edge to the adjacent list
+			indegree[w]++; //The number of edges entering vertex w is increased by 1
+			outdegree[v]++; //The number of edges leaving vertex v is decreased by 1
+			E++;
+			return 1; //Returns 1 if addEdge is successful
+		}
+		return -1; //Returns -1 if addEdge is unsuccessful
     }
     
     //Removes an edge from v to w
-    public void removeEdge(int v, int w){
+    public int removeEdge(int v, int w){
     	//Checking if vertices are in bounds
-    	checkVertex(v);
-    	checkVertex(w);
-    	adjList[v][w]=0; //Removing the edge from the adjacent list
-    	indegree[w]--; //The number of edges entering vertex w is decreased by 1
-    	outdegree[v]--; //The number of edges leaving vertex v is decreased by 1
-    	E--;
+    	if(checkVertex(v) == 1 && checkVertex(w) == 1) {;
+	    	adjList[v][w]=0; //Removing the edge from the adjacent list
+	    	indegree[w]--; //The number of edges entering vertex w is decreased by 1
+	    	outdegree[v]--; //The number of edges leaving vertex v is decreased by 1
+	    	E--;
+	    	return 1; //Returns 1 if removeEdge is successful
+    	}
+    	else return -1; //Returns -1 if removeEdge is unsuccessful
     }
     
     //returns outdegree (number of directed edges leaving) of vertex v
     public int outdegree(int v){
-    	checkVertex(v); //Checks if the vertex is in bounds
-    	return outdegree[v];
+    	if(checkVertex(v) == 1) return outdegree[v]; // successful
+    	else return -1; //unsuccessful
     }
     
     //returns indegree (number of directed edges leaving) of vertex v
     public int indegree(int v){
-    	checkVertex(v); //Checks if the vertex is in bounds
-    	return indegree[v];
+    	if(checkVertex(v) == 1) return indegree[v];//Checks if the vertex is in bounds
+    	else return -1; //unsuccessful
     }
     
     //returns the vertices adjacent from vertex v
     public int[] adjList(int v){
-    	checkVertex(v); //Checking if v is in bounds
-    	int[] adj = new int[outdegree[v]]; //Creates an Array of size outdegree of vertex v
-    	int count = 0;
-    	for(int i = 0; i < V; i++){
-    		if(adjList[v][i] == 1){ //If there is a directed edge between v and i, it is added to the adj list
-    			adj[count] = i;
-    			count++;
-    		}
+    	if(checkVertex(v) == 1) { //Checking if v is in bounds
+	    	int[] adj = new int[outdegree[v]]; //Creates an Array of size outdegree of vertex v
+	    	int count = 0;
+	    	for(int i = 0; i < V; i++){
+	    		if(adjList[v][i] == 1){ //If there is a directed edge between v and i, it is added to the adj list
+	    			adj[count] = i;
+	    			count++;
+	    		}
+	    	}
+	    	return adj;
     	}
-    	return adj;
+    	else return null;
+    	
     }
     
     //returns true if the graph contains a cycle, else false
@@ -117,45 +125,46 @@ public class DAG {
     //Found some nice code for LCA of DAG online, adjustments will be necessary to get my tests to work
     //This public function is used to find the LCA in a DAG
     public int findingLCA(int v, int w){
-    	checkVertex(v);
-    	checkVertex(w);
-    	if(E > 0 && !hasCycle()){ //If the DAG does not contain a cycle and has edges > 0, we can attempt to find the LCA
-    		int[] vArray = new int[E];
-    		int[] wArray = new int[E];
-    		boolean[] vMarked = new boolean[V];
-    		boolean[] wMarked = new boolean[V];
-    		int vCount =0;
-    		int wCount = 0;
-    		vArray[vCount] = v;
-    		wArray[wCount] = w;
-    		for(int j=0; j<V;j++){ //mark all vertices as not been visited yet
-    			vMarked[j] = false;
-    			wMarked[j] = false;
-    		}
-    		for(int i =0; i < V; i++){
-    			vMarked[v] = true;
-    			wMarked[w] = true;
-    			for(int j = 0; j < V; j++){
-    				if(adjList[i][j] == 1 && vMarked[i]){
-    					vCount++;
-    					vArray[vCount]=j;
-    					vMarked[j]=true;
-    				}
-    				if(adjList[i][j] == 1 && wMarked[i]){
-    					wCount++;
-    					wArray[wCount]=j;
-    					wMarked[j]=true;
-    				}
-    				if(wArray[wCount] == vArray[vCount]){
-    					return wArray[wCount];
-    				}
-    			}
-    		}
-    		return -1;//returns -1 if no ancestor found
+    	if(checkVertex(v) == 1 && checkVertex(w) == 1) {
+	    	if(E > 0 && !hasCycle()){ //If the DAG does not contain a cycle and has edges > 0, we can attempt to find the LCA
+	    		int[] vArray = new int[E];
+	    		int[] wArray = new int[E];
+	    		boolean[] vMarked = new boolean[V];
+	    		boolean[] wMarked = new boolean[V];
+	    		int vCount =0;
+	    		int wCount = 0;
+	    		vArray[vCount] = v;
+	    		wArray[wCount] = w;
+	    		for(int j=0; j<V;j++){ //mark all vertices as not been visited yet
+	    			vMarked[j] = false;
+	    			wMarked[j] = false;
+	    		}
+	    		for(int i =0; i < V; i++){
+	    			vMarked[v] = true;
+	    			wMarked[w] = true;
+	    			for(int j = 0; j < V; j++){
+	    				if(adjList[i][j] == 1 && vMarked[i]){
+	    					vCount++;
+	    					vArray[vCount]=j;
+	    					vMarked[j]=true;
+	    				}
+	    				if(adjList[i][j] == 1 && wMarked[i]){
+	    					wCount++;
+	    					wArray[wCount]=j;
+	    					wMarked[j]=true;
+	    				}
+	    				if(wArray[wCount] == vArray[vCount]){
+	    					return wArray[wCount];
+	    				}
+	    			}
+	    		}
+	    		return -1;//returns -1 if no ancestor found
+	    	}
+	    	else{
+	    		return -2;//returns -2 if graph has edges and has a cycle
+	    	}
     	}
-    	else{
-    		throw new IllegalArgumentException("This graph is not an acyclic graph - it may not contain any edges or have a cycle");
-    	}
+    	else return -3; //returns -3 if v or w are out of bounds
     }
     
 }
--- LCA/src/DAGTest.java
+++ LCA/src/DAGTest.java
@@ -7,15 +7,28 @@ import org.junit.jupiter.api.Test;
 class DAGTest {
 	
 	@Test
-	void testConstructor() {
+	void testConstructor() { //This will test the constructor and the vertices() function
 		//Test error case
 		assertEquals("Testing case for V < 0: ", -1, new DAG(-1).invalidV);
 		
 		//Test regular case (V = 0)
-		assertEquals("Testing case for V = 0", 0, new DAG(0).V);
+		assertEquals("Testing case for V = 0", 0, new DAG(0).vertices());
 		
 		//Test regular case (V > 0)
-		assertEquals("Testing case for V > 0", 4, new DAG(4).V);
+		assertEquals("Testing case for V > 0", 4, new DAG(4).vertices());
 	}
+	
+	@Test
+	void testCheckVertex() { //Tests the checkVertex() function
+		DAG dag1 = new DAG(0);
+		DAG dag2 = new DAG(3);
+		assertEquals("Testing v = -1: ", -1, dag2.checkVertex(-1));
+		assertEquals("Testing v = 0 when V = 0: ", -1, dag1.checkVertex(0));
+		assertEquals("Testing v = 0 when V > 0: ", 1, dag2.checkVertex(0));
+		assertEquals("Testing v > 0, v < V when V > 0: ", 1, dag2.checkVertex(2));
+		assertEquals("Testing v > V when V > 0: ", -1, dag2.checkVertex(4));
+	}
+	
+	
 
 }
